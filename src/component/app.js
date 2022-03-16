b54d714241d2e1e5bc27cd1d8935b515b5b46507
Jayson Morilla <60346390+jaysonmorilla@users.noreply.github.com>
2022-03-16 17:43:56 +0800

    Add files via upload
    Modified Files

--- src/component/app.js
+++ src/component/app.js
@@ -7,19 +7,25 @@ import {fab, faFacebookSquare, faLinkedinIn, faGithub} from "@fortawesome/free-b
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-
 library.add(fab, faFacebookSquare, faLinkedinIn, faGithub);
+
+const onClickHandler = ({ currentTarget }) => {
+    switch(currentTarget.id) {
+        case "github": window.open("https://github.com/jaysonmorilla"); break;
+        case "fb": window.open("https://facebook.com/jayson.morilla.39"); break;
+        case "linkedin": window.open("https://www.linkedin.com/in/jayson-morilla-615080201/"); break;
+        default: return;
+    }
+}
 
 AOS.init({
     duration : 2000
 })
-    
-const App = () => {
 
+const App = () => {
     window.addEventListener('DOMContentLoaded', () => {
         const animate = window.matchMedia("(max-width: 550px)").matches ? true : false;
         const elem = document.getElementsByClassName('animation');
-
         if(!animate){
             return;
         }
@@ -28,9 +34,7 @@ const App = () => {
             data.removeAttribute("data-aos");
         }
     });
-    
     const { state } = useContext(ThemeContext);
-
     return (
         <div className='home-page'>
             <div className='home-page-bg'>
@@ -43,60 +47,56 @@ const App = () => {
                     <div className='home-header-content'>
                         <section className='text'>
                             <h2>Hi, My name is <span style={{color: state.primary}}>Mark Jayson Morilla</span></h2>
-                            <h3>I'm a Front-End Web Developer.</h3>
+                            <h3>I am an aspiring Web Developer.</h3>
                             <button style={{border: `1px solid ${state.primary}`}} type='button'>Show Project</button>
                         </section>
                     </div>
                     <div id='content' className='content-container'>
                         <div className='graphics-content'>
-                            <section className='grph-text animation' data-aos='zoom-in-right'>
-                                <div className='grph-content'>
-                                    <h2 style={{color: state.primary}}>About</h2>
-                                    <h3>
-                                        I'm obsessed to do things especially when it comes to 
-                                        web development. I created a simple "System Project" in
-                                        school during my thesis in Software Development. It was
-                                        a Reservation System for renting a pool for an events. 
-                                        I love to work in the UI/UX of a website and especially,
-                                        I love to show my finish project to my fellow developer. 
-                                    </h3>
-                                </div>
-                            </section>
-                            <section className='log-text animation' data-aos='zoom-out-up'>
-                                <div className='log-content'>
-                                    <h2 style={{color: state.primary}}>Focus</h2>
-                                    <h3>
-                                        My specialty is about front-end web development 
-                                        using React js, making sketches turns into a nice
-                                        Semantic HTML & CSS application. I do a bit of 
-                                        Back-end using Webpack, Node js, Express js and
-                                        MySQL. I do sketching designs in Adobe Photoshop CS5.
-                                    </h3>
-                                </div>
-                            </section>
-                            <section className='art-text animation' data-aos='zoom-in-left'>
-                                <div className='art-content'>
-                                    <h2 style={{color: state.primary}}>Goal</h2>
-                                    <h3>
-                                        I'm planning to learn both Front-end and Back-end. 
-                                        In order to do that, I need to be exposed to a real
-                                        work and to be able to see, peoples loving my work.
-                                    </h3>
-                                </div>
-                            </section>
+                            <div className='graphics-content-flex'>
+                                <section className='grph-text animation' data-aos='zoom-in-right'>
+                                    <div className='grph-content'>
+                                        <div className='profile-img'></div>
+                                    </div>
+                                    <div className='contact-infos'>
+                                        <article style={{fontStyle: 'italic', fontWeight: 'lighter', textAlign: 'center'}}>
+                                            I am a college graduate with a Bachelor's Degree in Computer Engineer.
+                                        </article><br />
+                                        <hr /><br />
+                                        <div className='content-info'>
+                                            <article>
+                                                Email: jaysonmorilla2828@gmail.com
+                                            </article>
+                                            <article>
+                                                Phone: 09773765570
+                                            </article>
+                                        </div>
+                                    </div>
+                                </section>
+                                <section className='art-text animation' data-aos='zoom-in-left'>
+                                    <div className='art-content'>
+                                        <h2 style={{color: state.primary}}>I'm a Web Developer without job experience, But had some knowledge to develop a website.</h2>
+                                        <h3>
+                                         I do web development using <p style={{color: state.primary, fontWeight: 'bold', display: 'inline'}}>HTML, CSS, Javascript</p> as my front end development.
+                                         For my backend, I'm using <p style={{color: state.primary, fontWeight: 'bold', display: 'inline'}}>PHP</p> as well as <p style={{color: state.primary, fontWeight: 'bold', display: 'inline'}}>Mysql</p> for my database. I'm using CRUD methodology
+                                         in Object Oriented Programming structure and I have some knowledge about JSON.
+                                        </h3>
+                                    </div>
+                                </section>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='social-links'>
-                <div data-aos="zoom-out-right" className='fb animation'>
-                    <FontAwesomeIcon style={{ boxShadow: `0px 0px 0.5em ${state.bg}`, borderRadius: '50%' }} icon={['fab', 'facebook']} />
-                </div>
-                <div data-aos="zoom-out-center" className='github animation'>
-                    <FontAwesomeIcon style={{ boxShadow: `0px 0px 0.5em ${state.bg}`, borderRadius: '50%' }} icon={['fab', 'github']} />
-                </div>
-                <div data-aos="zoom-out-left" className='linkedin animation'>
-                    <FontAwesomeIcon style={{ boxShadow: `0px 0px 0.5em ${state.bg}`, borderRadius: '5%' }} icon={['fab', 'linkedin']} size="lg" />
+                <div className='social-links'>
+                    <div data-aos="zoom-out-right" className='fb animation'>
+                        <FontAwesomeIcon onClick={ onClickHandler } id='fb' style={{borderRadius: '50%' }} icon={['fab', 'facebook']} />
+                    </div>
+                    <div data-aos="zoom-out-center" className='github animation'>
+                        <FontAwesomeIcon onClick={ onClickHandler } id='github' style={{borderRadius: '50%' }} icon={['fab', 'github']} />
+                    </div>
+                    <div data-aos="zoom-out-left" className='linkedin animation'>
+                        <FontAwesomeIcon onClick={ onClickHandler } id='linkedin' style={{borderRadius: '5%' }} icon={['fab', 'linkedin']} size="lg" />
+                    </div>
                 </div>
             </div>
             <div className='languages'>
@@ -106,11 +106,11 @@ const App = () => {
                 <h4 style={{ textShadow: `0px 0px 18px ${state.bg}` }} >git</h4>
                 <h4 style={{ textShadow: `0px 0px 18px ${state.bg}` }} >node</h4>
                 <h4 style={{ textShadow: `0px 0px 18px ${state.bg}` }} >mysql</h4>
-                <h4 style={{ textShadow: `0px 0px 18px ${state.bg}` }} >webpack</h4>
+                <h4 style={{ textShadow: `0px 0px 18px ${state.bg}` }} >PHP</h4>
                 <h4 style={{ textShadow: `0px 0px 18px ${state.bg}` }} >react</h4>
             </div>
             <div className='footer'>
-                <h3>&copy; Jayson Morilla</h3>
+                <h3>&copy; Jayson Morilla</h3> 
             </div>
         </div>
     )
